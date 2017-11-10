8c6a7c4f18edda21443085a912e2dc99f755b5df
Valkir <valkir444@gmail.com>
2017-11-10 11:08:43 +0200

    merge solved

---
+++ js/jsAxmet.js
@@ -0,0 +1,6 @@
+var slideqty = $('#featured .item').length;
+
+for (var i = 0; i<slideqty; i++) {
+	var insertText = '<li data-target="#featured" data-slide-to="'+ i + '"> </li>';
+	$('#featured ol').append(insertText);
+}
